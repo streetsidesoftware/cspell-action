8781690049a6eb902e2fdfc040796c87c8d9d811
Jason Dent <jason@streetsidesoftware.nl>
2020-02-28 20:25:49 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
 
+// Deliberate spellling errorgs
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
