cad1f7b78bc2004cf86453d3226a0bac14c69720
Jason Dent <jason@streetsidesoftware.nl>
2020-02-28 20:00:08 +0100

    Initial checkin

---
+++ src/main.test.ts
@@ -0,0 +1,16 @@
+import * as process from 'process'
+import * as cp from 'child_process'
+import * as path from 'path'
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  process.env['INPUT_MILLISECONDS'] = '500'
+  const ip = path.join(__dirname, '..', 'dist', 'main.js')
+  const options: cp.ExecSyncOptions = {
+    env: process.env
+  }
+  const result = cp.execSync(`node ${ip}`, options).toString();
+  console.log(result);
+  expect(result).toEqual(expect.stringContaining('::debug::Waiting 500 milliseconds'));
+  expect(result).toEqual(expect.stringContaining('::set-output name=time::'));
+})
