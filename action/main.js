0387afa46b51e848da8919dadb13aa3e11a90b07
Jason Dent <jason@streetsidesoftware.nl>
2020-02-29 18:40:44 +0100

    Log to info

--- action/main.js
+++ action/main.js
@@ -6,11 +6,12 @@ const wait_1 = require("./wait");
 async function run() {
     try {
         const ms = core.getInput('milliseconds');
-        core.debug(`Waiting ${ms} milliseconds ...`);
+        core.info(`Waiting ${ms} milliseconds ...`);
         core.debug(new Date().toTimeString());
         await wait_1.wait(parseInt(ms, 10));
         core.debug(new Date().toTimeString());
         core.setOutput('time', new Date().toTimeString());
+        core.info('Done.');
     }
     catch (error) {
         core.setFailed(error.message);
--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import {wait} from './wait'
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.info(`Waiting ${ms} milliseconds ...`)
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
+    core.info('Done.')
   } catch (error) {
     core.setFailed(error.message)
   }
