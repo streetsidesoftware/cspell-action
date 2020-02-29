5ad22abe5ca10ccfb13bec47ba9a6c7c05fcff1a
Jason Dent <jason@streetsidesoftware.nl>
2020-02-29 19:27:52 +0100

    log the context

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import * as core from '@actions/core'
+import * as gitHub from '@actions/github'
 import {wait} from './wait'
 
 // Deliberate spelling errors
 async function run(): Promise<void> {
   try {
+    core.info('cspell-action')
+    const context = gitHub.context;
+    core.info(`context: ${JSON.stringify(context, null, 2)}`);
     const ms: string = core.getInput('milliseconds')
     core.info(`Waiting ${ms} milliseconds ...`)
 
