39dbe0b58f1b64a5bd8076b9fb2fba8993814a92
Jason Dent <jason@streetsidesoftware.nl>
2020-03-15 11:42:42 +0100

    refactor: address lint error

--- src/wait.test.ts
+++ src/wait.test.ts
@@ -9,6 +9,6 @@ test('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
   const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
+  const delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
 })
