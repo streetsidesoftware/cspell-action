05f8b2ba1d3017c044e6ee00b1de3496357344a8
Jason Dent <jason@streetsidesoftware.nl>
2020-03-22 12:48:24 +0100

    refactor main into its parts

--- action/main.js
+++ action/main.js
@@ -3,18 +3,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core = require("@actions/core");
 const gitHub = require("@actions/github");
 const wait_1 = require("./wait");
-// Deliberate spelling errors
+async function experiment() {
+    const ms = core.getInput('milliseconds');
+    core.info(`Waiting ${ms} milliseconds ...`);
+    core.debug(new Date().toTimeString());
+    await wait_1.wait(parseInt(ms, 10));
+    core.debug(new Date().toTimeString());
+    core.setOutput('time', new Date().toTimeString());
+}
+function pullRequest(context) {
+    core.info(`Pull Request: ${context.eventName}`);
+}
+function push(context) {
+    core.info(`Pull Request: ${context.eventName}`);
+}
+async function action() {
+    const context = gitHub.context;
+    core.info(`context: ${JSON.stringify(context, null, 2)}`);
+    switch (context.eventName) {
+        case 'push': return push(context);
+        case 'pull_request': return pullRequest(context);
+        default:
+            core.info(`Unknown event: '${context.eventName}'`);
+    }
+}
 async function run() {
     try {
         core.info('cspell-action');
-        const context = gitHub.context;
-        core.info(`context: ${JSON.stringify(context, null, 2)}`);
-        const ms = core.getInput('milliseconds');
-        core.info(`Waiting ${ms} milliseconds ...`);
-        core.debug(new Date().toTimeString());
-        await wait_1.wait(parseInt(ms, 10));
-        core.debug(new Date().toTimeString());
-        core.setOutput('time', new Date().toTimeString());
+        action();
+        experiment();
         core.info('Done.');
     }
     catch (error) {
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,44 @@ import * as core from '@actions/core'
 import * as gitHub from '@actions/github'
 import {wait} from './wait'
 
-// Deliberate spelling errors
+async function experiment() {
+  const ms: string = core.getInput('milliseconds')
+  core.info(`Waiting ${ms} milliseconds ...`)
+
+  core.debug(new Date().toTimeString())
+  await wait(parseInt(ms, 10))
+  core.debug(new Date().toTimeString())
+
+  core.setOutput('time', new Date().toTimeString())
+}
+
+type Context = typeof gitHub.context;
+
+function pullRequest(context: Context) {
+  core.info(`Pull Request: ${context.eventName}`)
+}
+
+function push(context: Context) {
+  core.info(`Pull Request: ${context.eventName}`)
+}
+
+async function action() {
+  const context = gitHub.context;
+  core.info(`context: ${JSON.stringify(context, null, 2)}`);
+
+  switch(context.eventName) {
+    case 'push': return push(context);
+    case 'pull_request': return pullRequest(context)
+    default:
+      core.info(`Unknown event: '${context.eventName}'`)
+  }
+}
+
 async function run(): Promise<void> {
   try {
     core.info('cspell-action')
-    const context = gitHub.context;
-    core.info(`context: ${JSON.stringify(context, null, 2)}`);
-    const ms: string = core.getInput('milliseconds')
-    core.info(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    action()
+    experiment()
     core.info('Done.')
   } catch (error) {
     core.setFailed(error.message)
