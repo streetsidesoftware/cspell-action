978e7b55340fb2aa0898aeb9ffb6c8441aa19c28
Jason Dent <Jason3S@users.noreply.github.com>
2020-04-05 11:07:45 +0200

    Fix install (#6)
    * Update package.json
    
    * Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,12 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.test.ts', '**/*.test.js'],
   testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
   browser: false,
-  roots: ['./src'],
+  roots: ['./src', './action'],
   verbose: true
 }
