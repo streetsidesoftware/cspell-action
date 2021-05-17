c77f6980ad3ce54060ccc93b797387fb8e60ff40
Jason Dent <Jason3S@users.noreply.github.com>
2021-05-17 14:28:54 +0200

    refactor: reduce the source depth (#167)
    * dev: hoist action-source to root
    * dev: remove old action modules
    * test: update tests
    * dev: move the fixtures to the right location
    * dev: Make sure the necessary node_modules are checked in
    * dev: remove dead source

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+  entry: './src/main_root.ts',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  node: {
+    __filename: false,
+    __dirname: false,
+  },
+  externalsType: 'commonjs-module',
+  externalsPresets: {
+    node: true,
+  },
+  externals: [
+    /^@cspell\/cspell-bundled-dicts/,
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'action'),
+  },
+};
