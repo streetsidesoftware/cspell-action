ad294cf8746d2fa74f9b60aa1cab7f81f736f7f3
Jason Dent <jason@streetsidesoftware.nl>
2021-06-03 18:00:22 +0200

    Delete test_main.js

--- action/lib/test_main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-const helper_1 = require("./test/helper");
-helper_1.pollyRun(__filename, `${process.env.FIXTURE_FILE_NAME}`, index_1.run);
-//# sourceMappingURL=test_main.js.map
