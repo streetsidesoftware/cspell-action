88b3c47286456c8c0d77bf80c3aada171626653c
Jason3S <Jason3S@users.noreply.github.com>
2023-03-13 12:07:43 +0000

    feat: Update CSpell to (6.29.1)

--- action/node_modules/cspell-trie-lib/dist/esm/lib/suggest.js
+++
@@ -1,4 +0,0 @@
-/**
- * This file is here to support code the referenced suggest directly and limit the exports.
- */
-export { genCompoundableSuggestions, genSuggestions, suggest } from './suggestions/suggest.js';
