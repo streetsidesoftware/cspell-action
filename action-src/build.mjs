78e59d184109fd5ab62f46ae403a5ec7aa6781a9
Jason Dent <Jason3S@users.noreply.github.com>
2023-10-17 12:12:42 +0200

    feat!: Require Node20 (#1400)

--- action-src/build.mjs
+++ action-src/build.mjs
@@ -1,20 +1,49 @@
 #!/usr/bin/env node
-/* eslint-disable node/no-unpublished-import */
 
 import * as esbuild from 'esbuild';
 import { fileURLToPath } from 'url';
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const targetPackageDir = '../action';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const srcPackageFile = path.resolve(__dirname, './package.json');
+const dstPackageFile = path.resolve(__dirname, targetPackageDir, 'package.json');
 
-async function buildAll() {
+async function buildLib() {
     // Note: cjs is the only possible option at this moment.
     await esbuild.build({
         absWorkingDir: __dirname,
         entryPoints: ['src/main_root.ts'],
         bundle: true,
         platform: 'node',
-        outfile: '../action/lib/main_root.js',
+        outfile: path.join(targetPackageDir, 'lib/main_root.js'),
     });
+}
+
+async function syncPackage() {
+    const pkgSrc = await readPackageJson(srcPackageFile);
+    const pkgDst = await readPackageJson(dstPackageFile);
+
+    const deps = pkgSrc['bundledDependencies'].map((dep) => [dep, pkgSrc.dependencies[dep]]);
+    pkgDst.dependencies = Object.fromEntries(deps);
+    pkgDst.bundledDependencies = pkgSrc.bundledDependencies;
+
+    await writePackageJson(dstPackageFile, pkgDst);
+}
+
+async function buildAll() {
+    await buildLib();
+    await syncPackage();
+}
+
+async function writePackageJson(pkgFile, pkgData) {
+    await fs.writeFile(pkgFile, JSON.stringify(pkgData, null, 2) + '\n');
+}
+
+async function readPackageJson(pkgFile) {
+    return JSON.parse(await fs.readFile(pkgFile, 'utf8'));
 }
 
 buildAll();
