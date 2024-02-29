453a17513fa54504f82f73fe6e7114128428551f
Jason Dent <jason@streetsidesoftware.nl>
2024-02-29 13:14:47 +0100

    Update main.test.ts

--- action-src/src/main.test.ts
+++ action-src/src/main.test.ts
@@ -5,13 +5,11 @@ import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 
 const timeout = 20000;
 
-console.log('%o', { ...helper });
-
 const spyStdout = vi.spyOn(process.stdout, 'write').mockImplementation(function () {
     return true;
 });
 
-describe.only('Validate Main', () => {
+describe('Validate Main', () => {
     beforeEach(() => {
         spyStdout.mockClear();
     });
@@ -35,35 +33,6 @@ describe.only('Validate Main', () => {
             Object.assign(process.env, env);
 
             await expect(run()).resolves.toBeUndefined();
-        },
-        timeout,
-    );
-});
-
-describe('Validate Main No Token', () => {
-    beforeEach(() => {
-        vi.spyOn(console, 'error').mockImplementation(() => undefined);
-    });
-
-    afterEach(() => {
-        vi.resetAllMocks();
-    });
-
-    test.each`
-        test                                       | file
-        ${'event pull_request main.js'}            | ${'pull_request.json'}
-        ${'event pull_request_with_files main.js'} | ${'pull_request_with_files.json'}
-    `(
-        '$test',
-        async ({ file }) => {
-            const env = helper.fetchGithubActionFixture(file);
-            env.FIXTURE_FILE_NAME = file;
-            Object.assign(process.env, env);
-
-            process.env['INPUT_GITHUB_TOKEN'] = undefined;
-            process.env['GITHUB_TOKEN'] = undefined;
-
-            await expect(run()).resolves.toEqual(expect.any(Error));
         },
         timeout,
     );
