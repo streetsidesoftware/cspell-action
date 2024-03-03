604dd6f9b4562d72eb0544c8515117b5f5713bd2
Jason Dent <jason@streetsidesoftware.nl>
2024-03-03 14:19:21 +0100

    add tests

--- action-src/src/error.test.ts
+++ action-src/src/error.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { AppError, isError, toError } from './error.js';
+import { AppError, isAppError, isError, toError } from './error.js';
 
 describe('error', () => {
     test.each`
@@ -12,5 +12,31 @@ describe('error', () => {
         ${null}                      | ${false}
     `('isError $value', ({ value, expected }) => {
         expect(isError(value)).toBe(expected);
+    });
+
+    test.each`
+        value                        | expected
+        ${Error('hello')}            | ${Error('hello')}
+        ${new AppError('app error')} | ${Error('app error')}
+        ${toError('hello')}          | ${Error('hello')}
+        ${'hello'}                   | ${Error('hello')}
+        ${{}}                        | ${Error('Unknown error')}
+        ${null}                      | ${Error('Unknown error')}
+    `('toError $value', ({ value, expected }) => {
+        expect(toError(value)).toEqual(expected);
+    });
+
+    test('AppError', () => {
+        const err = new AppError('app error');
+        expect(err).toBeInstanceOf(Error);
+        expect(err).toBeInstanceOf(AppError);
+        expect(err.message).toBe('app error');
+    });
+
+    test('isAppError', () => {
+        const err = new AppError('app error');
+        expect(isAppError(err)).toBe(true);
+        expect(isAppError(Error('app error'))).toBe(false);
+        expect(isAppError('app error')).toBe(false);
     });
 });
