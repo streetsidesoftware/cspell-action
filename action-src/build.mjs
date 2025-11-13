dd5f746a5acf3c45850912bb5a03f4c193183669
Jason Dent <jason@streetsidesoftware.nl>
2025-11-13 07:58:10 +0100

    Add ts-check

--- action-src/build.mjs
+++ action-src/build.mjs
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// @ts-check
+
 import * as esbuild from 'esbuild';
 import { promises as fs } from 'fs';
 import path from 'path';
@@ -25,30 +27,60 @@ async function buildLib() {
         },
         outfile: path.join(targetPackageDir, 'lib/main_root.cjs'),
         target: 'node24',
-        supported: { 'regexp-unicode-property-escapes': true },
+        supported: {
+            'regexp-dot-all-flag': true,
+            'regexp-lookbehind-assertions': true,
+            'regexp-match-indices': true,
+            'regexp-named-capture-groups': true,
+            'regexp-set-notation': true,
+            'regexp-sticky-and-unicode-flags': true,
+            'regexp-unicode-property-escapes': true,
+        },
     });
 }
 
+/**
+ * @typedef {Object} PkgFile
+ * @property {string[]} bundleDependencies
+ * @property {Record<string, string>} dependencies
+ */
+
+/**
+ * @returns {Promise<void>}
+ */
 async function syncPackage() {
     const pkgSrc = await readPackageJson(srcPackageFile);
     const pkgDst = await readPackageJson(dstPackageFile);
 
-    const deps = pkgSrc['bundledDependencies'].map((dep) => [dep, pkgSrc.dependencies[dep]]);
+    const deps = pkgSrc['bundleDependencies'].map((dep) => [dep, pkgSrc.dependencies[dep]]);
     pkgDst.dependencies = Object.fromEntries(deps);
-    pkgDst.bundledDependencies = pkgSrc.bundledDependencies;
+    pkgDst.bundleDependencies = pkgSrc.bundleDependencies;
 
     await writePackageJson(dstPackageFile, pkgDst);
 }
 
+/**
+ * @returns {Promise<void>}
+ */
 async function buildAll() {
     await buildLib();
     await syncPackage();
 }
 
+/**
+ *
+ * @param {string} pkgFile
+ * @param {PkgFile} pkgData
+ */
 async function writePackageJson(pkgFile, pkgData) {
     await fs.writeFile(pkgFile, JSON.stringify(pkgData, null, 2) + '\n');
 }
 
+/**
+ *
+ * @param {string} pkgFile
+ * @returns {Promise<PkgFile>}
+ */
 async function readPackageJson(pkgFile) {
     return JSON.parse(await fs.readFile(pkgFile, 'utf8'));
 }
